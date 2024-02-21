2aae1cac457e669e6fac8dde1e32840fd25d19e7
CrisMuniz <cristinamuniz85@gmail.com>
2024-02-21 11:22:48 +0100

    add cursor image and inputSearch hover

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,7 +3,7 @@ import './styles/TodoSearch.css';
 function TodoSearch() {
     return(
         <form className="formSearch">
-        <input className="inputSearch" placeholder="Picar cebolla" />
+        <input className="inputSearch" placeholder="Ej.Picar cebolla" />
         </form>
     );
 }
